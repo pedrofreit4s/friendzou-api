b633db7ac5ca5ee19fef14bab08d07788a1f7eb5
Pedro Freitas <pedrofreit4s12@gmail.com>
2022-12-28 01:22:54 -0300

    cors

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import cors from "cors";
 import { v1Routes } from "./routes/v1.routes";
 
 const server = express();
 const serverPort = Number(process.env.APP_PORT) || 8080;
 
+server.use(cors());
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 server.use("/api/v1", v1Routes);
